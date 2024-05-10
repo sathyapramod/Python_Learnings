d9c642f65a94d089b96c35e90764eb4bfc88e7a3
sathyapr <sathyapramod12@gmail.com>
2024-05-11 00:05:36 +0530

    Varibale assignment and basic data types

--- hello.py
+++ hello.py
@@ -3,3 +3,36 @@ Write a program to print hello world! In the console
 """
 
 print("Hello World!")
+
+"""
+Creating variable and assigning values
+<variable name> = <value>
+"""
+
+# Integer
+a = 2
+print(a)
+print(type(a))
+
+# Floating point
+pi = 3.14
+print(pi)
+print(type(pi))
+
+# String
+b = 'A'
+print(b)
+print(type(b))
+
+name = 'Sathya Pramod'
+print(name)
+
+# Boolean
+q = True
+print(q)
+print(type(q))
+
+# Empty or null
+x = None
+print(x)
+print(type(x))
